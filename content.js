a652424f0b139d043012e96a4a3c49aeb28de544
taichi.amano <taichi.amano@speee.jp>
2018-06-13 11:58:49 +0900

    Fix format

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 class XxParadise {
   constructor(config) {
@@ -6,44 +6,55 @@ class XxParadise {
   }
 
   build() {
-    document.addEventListener('DOMContentLoaded', () => {
-      const imgs = document.getElementsByTagName('img');
-      this.replaceImageUrls(imgs);
-    }, false);
-    document.addEventListener('DOMNodeInserted', (e) => {
-      if (e.target instanceof HTMLElement) {
-        const imgs = e.target.querySelectorAll('img');
+    document.addEventListener(
+      "DOMContentLoaded",
+      () => {
+        const imgs = document.getElementsByTagName("img");
         this.replaceImageUrls(imgs);
-      }
-    }, false);
+      },
+      false
+    );
+    document.addEventListener(
+      "DOMNodeInserted",
+      e => {
+        if (e.target instanceof HTMLElement) {
+          const imgs = e.target.querySelectorAll("img");
+          this.replaceImageUrls(imgs);
+        }
+      },
+      false
+    );
   }
 
   replaceImageUrls(imgs) {
-    Array.prototype.forEach.call(imgs, (img) => {
+    Array.prototype.forEach.call(imgs, img => {
       if (img.src.match(new RegExp(this.config.activeSrcRegexp))) {
         const width = img.width;
         const height = img.height;
         if (width > 0 && height > 0) {
           const fingerprint = this.getFingerprint(img.src);
-          img.src = '';
-          img.src = this.buildImageUrl(width, height, fingerprint);
+          const imageUrl = this.buildImageUrl(width, height, fingerprint);
+          img.src = "";
+          img.src = imageUrl;
         }
       }
     });
   }
 
   buildImageUrl(width, height, fingerprint) {
-    let parameter = width + 'x' + height;
-    if (this.config.mode === 'aggressive_privacy')
-      parameter += '-mode_aggressive_privacy';
-    return '//' + this.config.endpointDomain + '/' + parameter + '/' + fingerprint;
+    let parameter = width + "x" + height;
+    if (this.config.mode === "aggressive_privacy")
+      parameter += "-mode_aggressive_privacy";
+    return (
+      "//" + this.config.endpointDomain + "/" + parameter + "/" + fingerprint
+    );
   }
 
   getFingerprint(str) {
-    str = str.replace(/[^abcdefghijklmnopqrstuvwxyz0123456789]/gi, '');
+    str = str.replace(/[^abcdefghijklmnopqrstuvwxyz0123456789]/gi, "");
     const sLength = str.length;
-    let fingerprint = '';
-    for(var i = 0; i < 32; i++){
+    let fingerprint = "";
+    for (var i = 0; i < 32; i++) {
       let t = str[i * 2];
       if (t) fingerprint += t;
       else break;
@@ -52,8 +63,11 @@ class XxParadise {
   }
 }
 
-Config.read().then((config) => {
-  if (config.status === 'enabled' && document.domain.match(new RegExp(config.activeDomainRegexp))) {
-    (new XxParadise(config)).build();
+Config.read().then(config => {
+  if (
+    config.status === "enabled" &&
+    document.domain.match(new RegExp(config.activeDomainRegexp))
+  ) {
+    new XxParadise(config).build();
   }
 });
